c89cba894ec9e86b3c6f7d7e500d8e19f85a0398
warley996 <salsadelmonte@hotmail.com>
2017-09-06 10:40:48 -0600

    test changes

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,10 @@
     <div>
       <logo/>
       <h1 class="title">
-        helloworld
+        Hola Mundo!
       </h1>
       <h2 class="subtitle">
-        My first application with vue and nuxt
+        Calacion de Netlify...
       </h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
